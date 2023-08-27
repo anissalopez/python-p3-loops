7a5294063c202b70163609f480579b812702930b
anissalopez <anissa965@gmail.com>
2023-08-27 17:02:39 -0400

    working with loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python3
+import ipdb
 
 def happy_new_year():
-    # code goes here!
+    num = 10
+    while (num > 0):
+        print(num)
+        num -= 1
+    if(num == 0):
+        print("Happy New Year!")
+
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+
+    new_list = list()
+    for num in int_list:
+        newNum =  num * num
+        new_list.append(newNum)
+
+    return new_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+
+
+    while (num <= 100):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else:
+            print(num)
+        num += 1
+    
+print(fizzbuzz())
